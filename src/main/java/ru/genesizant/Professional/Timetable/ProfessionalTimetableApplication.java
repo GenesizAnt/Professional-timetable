190f001c22bb383248a6afb9b10c104b7a401251
Anton <genesiz1990@gmail.com>
2023-11-13 19:51:39 +0300

    first

---
+++ src/main/java/ru/genesizant/Professional/Timetable/ProfessionalTimetableApplication.java
@@ -0,0 +1,13 @@
+package ru.genesizant.Professional.Timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalTimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalTimetableApplication.class, args);
+	}
+
+}
