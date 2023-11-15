98f8f6570979438dbc40e9c24650855c71f8a78c
Anton <genesiz1990@gmail.com>
2023-11-15 19:42:54 +0300

    add custom authorization page

--- src/main/java/ru/genesizant/Professional/Timetable/config/SecurityConfig.java
+++ src/main/java/ru/genesizant/Professional/Timetable/config/SecurityConfig.java
@@ -4,10 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ru.genesizant.Professional.Timetable.services.PersonDetailsService;
 
 @Configuration
@@ -19,6 +24,31 @@ public class SecurityConfig {
 
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(CsrfConfigurer::disable) //если не отправляется токен с формы
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/super").hasRole("SUPER")
+                        .requestMatchers("/admin").hasAnyRole("ADMIN", "SUPER")
+                        .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
+                        .anyRequest().hasAnyRole("USER", "ADMIN", "SUPER"))
+                .formLogin(login -> login
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/hello", true)
+                        .failureUrl("/auth/login?error"));
+//                        .permitAll());
+//                .logout((logout) -> logout
+//                        .logoutUrl("/logout")
+//                        .logoutSuccessUrl("/auth/login")
+//                        .permitAll())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));//не сохраняет сессию на сервере
+
+//        httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
     }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
