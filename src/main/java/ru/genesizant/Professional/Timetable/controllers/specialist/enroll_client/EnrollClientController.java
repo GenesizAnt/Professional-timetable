610b6ec01032499a662cc1432d53aee32a0c43ff
Anton <genesiz1990@gmail.com>
2023-12-05 22:49:53 +0300

    add class controller EnrollClientController

--- src/main/java/ru/genesizant/Professional/Timetable/controllers/specialist/enroll_client/EnrollClientController.java
+++ src/main/java/ru/genesizant/Professional/Timetable/controllers/specialist/enroll_client/EnrollClientController.java
@@ -1,10 +1,16 @@
-package ru.genesizant.Professional.Timetable.controllers.specialist;
+package ru.genesizant.Professional.Timetable.controllers.specialist.enroll_client;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.genesizant.Professional.Timetable.controllers.specialist.calendar.StatusAdmissionTime;
 import ru.genesizant.Professional.Timetable.model.Person;
 import ru.genesizant.Professional.Timetable.security.JWTUtil;
 import ru.genesizant.Professional.Timetable.services.DatesAppointmentsService;
@@ -12,56 +18,30 @@ import ru.genesizant.Professional.Timetable.services.PersonService;
 
 import java.time.LocalDate;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/specialist")
-public class SpecialistController {
+@RequestMapping("/enroll")
+public class EnrollClientController {
 
     private final JWTUtil jwtUtil;
     private final PersonService personService;
     private final DatesAppointmentsService datesAppointmentsService;
 
     @Autowired
-    public SpecialistController(JWTUtil jwtUtil, PersonService personService, DatesAppointmentsService datesAppointmentsService) {
+    public EnrollClientController(JWTUtil jwtUtil, PersonService personService, DatesAppointmentsService datesAppointmentsService) {
         this.jwtUtil = jwtUtil;
         this.personService = personService;
         this.datesAppointmentsService = datesAppointmentsService;
     }
 
-    @GetMapping("/start_menu_specialist")
-    public String getStartMenu(Model model, HttpServletRequest request) {
-
-        if (jwtUtil.isValidJWTAndSession(request)) {
-
-            model.addAttribute("name", request.getSession().getAttribute("name"));
-
-//            List<Person> specialists = personService.getPersonByRoleList("ROLE_ADMIN");
-
-            model.addAttribute("name", request.getSession().getAttribute("name"));
-//            model.addAttribute("specialists", specialists);
-//            (long) request.getSession().getAttribute("id")
-
-
-        } else {
-            model.addAttribute("error", "Упс! Пора перелогиниться!");
-            return "redirect:/auth/login?error";
-        }
-
-        return "specialist/start_menu_specialist";
-    }
-
-    @GetMapping("/admission_calendar_view")
-    public String addAdmissionCalendarView(Model model, HttpServletRequest request) {
-
+    @GetMapping("/enroll_page")
+    public String addAdmissionCalendarUpdate(Model model, HttpServletRequest request) {
         if (jwtUtil.isValidJWTAndSession(request)) {
 
             Map<LocalDate, Map<String, String>> datesNotSorted = datesAppointmentsService.getCalendarFreeScheduleById((long) request.getSession().getAttribute("id"));
-
-
 
             // Создаем новую Map для хранения отсортированных значений
             Map<LocalDate, Map<String, String>> datesNoSortedDate = new LinkedHashMap<>();
@@ -87,7 +67,7 @@ public class SpecialistController {
             return "redirect:/auth/login?error";
         }
 
-        return "specialist/admission_calendar_view";
+        //                return "redirect:/specialist/enroll_client_view";
+        return "specialist/enroll_client_view";
     }
-
 }
---
+++ src/main/resources/templates/specialist/resources/templates/specialist/test.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.dataTable').DataTable({
+        "paging": true,
+        "pageLength": 5
+    });
+});
