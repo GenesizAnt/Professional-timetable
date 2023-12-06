d73b2f84ce601698944b9447eb89b959082399bd
Anton <genesiz1990@gmail.com>
2023-12-06 18:45:19 +0300

    add html Enroll Client view

---
+++ src/main/java/ru/genesizant/Professional/Timetable/model/SpecialistsAndClients.java
@@ -0,0 +1,4 @@
+package ru.genesizant.Professional.Timetable.model;
+
+public class SpecialistsAndClients {
+}
--- src/main/resources/templates/specialist/resources/templates/specialist/test.js
+++ src/main/resources/templates/specialist/resources/templates/specialist/test.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.dataTable').DataTable({
+    $('table').DataTable({
         "paging": true,
         "pageLength": 5
     });
